589c8262cbb90b4e9deaef88d0fdab51a26caf2a
HamzaBari2000 <67334675+HamzaBari2000@users.noreply.github.com>
2020-11-17 02:58:44 +0000

    Commit 13 - Formatting Code

--- js/Lessons.js
+++ js/Lessons.js
@@ -1,7 +1,6 @@
 //The JSON Data Array For Lessons.
 let lessons = [
   {
-    id: 1,
     SubjectName: "Maths",
     Location: "London",
     Price: 50.0,
@@ -9,7 +8,6 @@ let lessons = [
     Image: "img/maths.svg",
   },
   {
-    id: 2,
     SubjectName: "English",
     Location: "Manchester",
     Price: 30.0,
@@ -17,7 +15,6 @@ let lessons = [
     Image: "img/english.png",
   },
   {
-    id: 3,
     SubjectName: "Science",
     Location: "New York",
     Price: 75.0,
@@ -25,7 +22,6 @@ let lessons = [
     Image: "img/science.png",
   },
   {
-    id: 4,
     SubjectName: "Computer Science",
     Location: "Birmingham",
     Price: 100.0,
@@ -33,7 +29,6 @@ let lessons = [
     Image: "img/cs.jpg",
   },
   {
-    id: 5,
     SubjectName: "History",
     Location: "London",
     Price: 85.0,
@@ -41,7 +36,6 @@ let lessons = [
     Image: "img/history.png",
   },
   {
-    id: 6,
     SubjectName: "Geography",
     Location: "New York",
     Price: 85.0,
@@ -49,7 +43,6 @@ let lessons = [
     Image: "img/geography.png",
   },
   {
-    id: 7,
     SubjectName: "Religious Education",
     Location: "Leeds",
     Price: 80.0,
@@ -57,7 +50,6 @@ let lessons = [
     Image: "img/Re.png",
   },
   {
-    id: 8,
     SubjectName: "Phyical Education",
     Location: "Reading",
     Price: 110.0,
@@ -65,7 +57,6 @@ let lessons = [
     Image: "img/PhyicalEducation.png",
   },
   {
-    id: 9,
     SubjectName: "Business",
     Location: "Cape Town",
     Price: 145.0,
@@ -73,7 +64,6 @@ let lessons = [
     Image: "img/business.png",
   },
   {
-    id: 10,
     SubjectName: "Information Technology",
     Location: "Amsterdam",
     Price: 25.0,
