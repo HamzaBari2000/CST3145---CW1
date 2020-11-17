e44e77ba6ff77945adcd632e18c62613ef317682
HamzaBari2000 <67334675+HamzaBari2000@users.noreply.github.com>
2020-11-17 02:51:20 +0000

    Commit 12 - Index Error Figured Out

--- js/Lessons.js
+++ js/Lessons.js
@@ -1,6 +1,7 @@
 //The JSON Data Array For Lessons.
 let lessons = [
   {
+    id: 1,
     SubjectName: "Maths",
     Location: "London",
     Price: 50.0,
@@ -8,6 +9,7 @@ let lessons = [
     Image: "img/maths.svg",
   },
   {
+    id: 2,
     SubjectName: "English",
     Location: "Manchester",
     Price: 30.0,
@@ -15,6 +17,7 @@ let lessons = [
     Image: "img/english.png",
   },
   {
+    id: 3,
     SubjectName: "Science",
     Location: "New York",
     Price: 75.0,
@@ -22,6 +25,7 @@ let lessons = [
     Image: "img/science.png",
   },
   {
+    id: 4,
     SubjectName: "Computer Science",
     Location: "Birmingham",
     Price: 100.0,
@@ -29,6 +33,7 @@ let lessons = [
     Image: "img/cs.jpg",
   },
   {
+    id: 5,
     SubjectName: "History",
     Location: "London",
     Price: 85.0,
@@ -36,6 +41,7 @@ let lessons = [
     Image: "img/history.png",
   },
   {
+    id: 6,
     SubjectName: "Geography",
     Location: "New York",
     Price: 85.0,
@@ -43,6 +49,7 @@ let lessons = [
     Image: "img/geography.png",
   },
   {
+    id: 7,
     SubjectName: "Religious Education",
     Location: "Leeds",
     Price: 80.0,
@@ -50,6 +57,7 @@ let lessons = [
     Image: "img/Re.png",
   },
   {
+    id: 8,
     SubjectName: "Phyical Education",
     Location: "Reading",
     Price: 110.0,
@@ -57,6 +65,7 @@ let lessons = [
     Image: "img/PhyicalEducation.png",
   },
   {
+    id: 9,
     SubjectName: "Business",
     Location: "Cape Town",
     Price: 145.0,
@@ -64,6 +73,7 @@ let lessons = [
     Image: "img/business.png",
   },
   {
+    id: 10,
     SubjectName: "Information Technology",
     Location: "Amsterdam",
     Price: 25.0,
@@ -74,3 +84,27 @@ let lessons = [
 
 //Store Data to the Local Storage
 localStorage.setItem("Lessons", JSON.stringify(lessons));
+
+/* 
+
+case 9:
+                            if (this.sortOption === 'name') {
+                                this.items.sort(function (x, y) {
+                                    if (x.name.toLowerCase() < y.name.toLowerCase()) {
+                                        return -1;
+                                    }
+                                    else if (x.name.toLowerCase() > y.name.toLowerCase()) {
+                                        return 1;
+                                    }
+                                    else {
+                                        return 0;
+                                    }
+                                });
+                            }
+                            else {
+                                this.items.sort((x, y) => x.id - y.id);
+                            }
+
+                            return this.items;
+
+*/
